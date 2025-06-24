70decae2dd6cddc9126eff4437fd9777920592a3
Vishesh Nagar <nagarvishesh16@gmail.com>
2025-06-24 20:52:22 +0530

    Update project name to 'showzy' and improve layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,16 @@
 import { Stack } from "expo-router";
-import './globals.css';
+import "./globals.css";
 
 export default function RootLayout() {
-    return <Stack screenOptions={{
-        headerTitleAlign: "center",  // 👈 this line centers the title
-      }}/>;
+    return (
+        <Stack 
+            screenOptions={{
+                headerTitleAlign: "center",
+                headerStyle: {
+                    backgroundColor: "#1f2937",
+                },
+                headerTintColor: "#f3f4f6",
+            }}
+        />
+    );
 }

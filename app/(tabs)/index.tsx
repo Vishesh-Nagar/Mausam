74644d11727fc6e654609b9e0a0dd0fab3272f67
Vishesh Nagar <nagarvishesh16@gmail.com>
2025-07-01 01:29:51 +0530

    Fetching and displaying movies from custom hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,28 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, Text, View } from "react-native";
+import {
+    ActivityIndicator,
+    FlatList,
+    Image,
+    ScrollView,
+    Text,
+    View,
+} from "react-native";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 export default function Index() {
     const router = useRouter();
+
+    const {
+        data: movies,
+        loading: moviesLoading,
+        error: moviesError,
+    } = useFetch(() => fetchMovies({ query: "" }));
+
     return (
         <View className="flex-1 bg-primary">
             <Image source={images.bg} className="absolute w-full z-0" />
@@ -18,12 +35,46 @@ export default function Index() {
                     source={icons.logo}
                     className="w-12 h-10 mt-20 mb-5 mx-auto"
                 ></Image>
-                <View className="flex-1 mt-5">
-                    <SearchBar
-                        onPress={() => router.push("/search")}
-                        placeholder="Search for a movie"
+                {moviesLoading ? (
+                    <ActivityIndicator
+                        size="large"
+                        color="#fff"
+                        className="mt-10 self-center"
                     />
-                </View>
+                ) : moviesError ? (
+                    <Text className="text-white text-center mt-10">
+                        {moviesError.message}
+                    </Text>
+                ) : (
+                    <View className="flex-1 mt-5">
+                        <SearchBar
+                            onPress={() => router.push("/search")}
+                            placeholder="Search for a movie"
+                        />
+                        <>
+                            <Text className="text-white text-lg font-bold mt-5 mb-3">
+                                Trending Movies
+                            </Text>
+                            <FlatList
+                                data={movies}
+                                renderItem={({ item }) => (
+                                    <MovieCard {...item} />
+                                )}
+                                showsVerticalScrollIndicator={false}
+                                keyExtractor={(item) => item.id.toString()}
+                                numColumns={3}
+                                columnWrapperStyle={{
+                                    justifyContent: "flex-start",
+                                    gap: 20,
+                                    paddingRight: 5,
+                                    marginBottom: 10,
+                                }}
+                                className="mt-2 pb-32"
+                                scrollEnabled={false}
+                            />
+                        </>
+                    </View>
+                )}
             </ScrollView>
         </View>
     );

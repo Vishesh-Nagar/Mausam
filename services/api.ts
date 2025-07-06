a1b7be2d60bdc721b807a59779e735e26a5bcb9e
Vishesh Nagar <nagarvishesh16@gmail.com>
2025-07-06 15:21:26 +0530

    Setup the movie detail card

--- services/api.ts
+++ services/api.ts
@@ -9,7 +9,9 @@ export const TMDB_CONFIG = {
 
 export const fetchMovies = async ({ query }: { query: string }) => {
     const endpoint = query
-        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(
+              query
+          )}`
         : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     const res = await fetch(endpoint, {
@@ -26,17 +28,27 @@ export const fetchMovies = async ({ query }: { query: string }) => {
     return data.results;
 };
 
-// const url = "https://api.themoviedb.org/3/authentication";
-// const options = {
-//     method: "GET",
-//     headers: {
-//         accept: "application/json",
-//         Authorization:
-//             "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTIxODMxNDEwM2ZlZjk0ZTE1ZjFlMzc4MWUxNzRhYSIsIm5iZiI6MTc1MTI4MDg5Ny40ODE5OTk5LCJzdWIiOiI2ODYyNmQwMWRhN2ExZDhjYmEyMjlhMzQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.qZGf57YrcN7uwqNeW-36FwnHZbncn10XnhjA-ESW4_s",
-//     },
-// };
+export const fetchMovieDetails = async (
+    movieId: string
+): Promise<MovieDetails> => {
+    try {
+        const response = await fetch(
+            `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+            {
+                method: "GET",
+                headers: TMDB_CONFIG.headers,
+            }
+        );
 
-// fetch(url, options)
-//     .then((res) => res.json())
-//     .then((json) => console.log(json))
-//     .catch((err) => console.error(err));
+        if (!response.ok) {
+            // @ts-ignore
+            throw new Error("Failed to fetch movie details");
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching movie details:", error);
+        throw error;
+    }
+};
